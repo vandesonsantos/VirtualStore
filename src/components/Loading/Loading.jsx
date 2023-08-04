9c4f1d07141268dd911562ac4b14219334b6ce6c
Vandeson Santos <vandeson.eng.quimica@gmail.com>
2023-08-04 17:22:44 -0300

    Component Loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AiOutlineReload } from 'react-icons/ai';
+
+import './Loading.css';
+
+const Loading = () => {
+  return  (
+    <div>
+      <AiOutlineReload className="loading" />
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,23 +4,25 @@ import React, { useState, useEffect } from 'react';
 import './Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductsCard from '../productCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 function Products() {
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     fetchProducts('game').then((response) => {
       setProducts(response);
+      setLoading(false);
     });
   }, []);
 
   return (
-    <section className="products conteiner">
-      {
-        products.map((product) => <ProductsCard key={product.id} data={product} />)
-      }
-    </section>
+    (loading && <Loading />) || (<section className="products conteiner">
+      {products.map((product) => <ProductsCard key={product.id} data={product} />)}
+    </section>)
   );
 }
 
